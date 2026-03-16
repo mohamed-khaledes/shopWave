c6ccb4ab6fc485d2d54af11947b9409246f8f97b
mohamed khaled <mohamedkhaelsayed@gmail.com>
2026-03-16 11:02:49 +0200

    fix some issues in auth redirections

--- providers/StoreProvider.tsx
+++ providers/StoreProvider.tsx
@@ -5,14 +5,18 @@ import { useEffect } from 'react'
 import { restoreAuth } from '@/store/authSlice'
 import { getToken, getCachedUser } from '@/lib/auth'
 import type { User } from '@/types'
+import { useRouter } from 'next/navigation'
 
 /** Restores auth state from cookies/localStorage on mount */
 function AuthRestorer() {
+  const router = useRouter()
   useEffect(() => {
     const token = getToken()
     const user = getCachedUser() as User | null
     if (token && user) {
       store.dispatch(restoreAuth({ user, token }))
+    } else {
+      router.push('/login')
     }
   }, [])
   return null
